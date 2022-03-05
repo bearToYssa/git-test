32676af812f1edb99f0788a98cb40bbc2f67ff90
bear <bear@yssa.com>
2022-03-05 19:44:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Description: Git测试
+ * @Author: Bear
+ * @CreateTime: 3/5/2022 19:37
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
